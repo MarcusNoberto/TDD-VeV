9bf1b36039150d161e8611f3429d46d17fad7c9a
johanssonlucena <johanssonlucena@gmail.com>
2023-08-21 20:16:02 -0300

    Nota Fiscal ok

--- geradorNotaFiscal/gerador/NotaFiscal.java
+++ geradorNotaFiscal/gerador/NotaFiscal.java
@@ -8,6 +8,13 @@ public class NotaFiscal {
 	
 	public NotaFiscal (String nomeCliente, double valorNota, double valorImposto) {
 		
+		if (nomeCliente == null || valorNota <= 0 || valorImposto <= 0) {
+			throw new NullPointerException("Informações não podem ser nulos.");
+		}
+		if (nomeCliente.isBlank()) {
+			throw new IllegalArgumentException("Informações não podem ser vazios.");
+		}
+		
 		this.nomeCliente = nomeCliente;
 		this.valorNota = valorNota;
 		this.valorImposto = valorImposto;
@@ -17,9 +24,43 @@ public class NotaFiscal {
 		return nomeCliente;
 	}
 
-	public void setNomeCliente(String nomeCliente) {		
+	public void setNomeCliente(String nomeCliente) {
+		if (nomeCliente == null) {
+			throw new NullPointerException("Informações não podem ser nulos.");
+		}
+		if (nomeCliente.isBlank()) {
+			throw new IllegalArgumentException("Informações não podem ser vazios.");
+		}
+		
 		this.nomeCliente = nomeCliente;
 	}
+	public double getValorNota() {
+		return valorNota;
+	}
+
+	public void setValorNota(double valorNota) {
+		if (valorNota <= 0) {
+			throw new NullPointerException("Informações não podem ser nulos.");
+		}
 		
+		this.valorNota = valorNota;
+	}
+
+	public double getValorImposto() {
+		return valorImposto;
+	}
+
+	public void setValorImposto(double valorImposto) {
+		if (valorNota <= 0) {
+			throw new NullPointerException("Informações não podem ser nulos.");
+		}
+		
+		this.valorImposto = valorImposto;
+	}
+
+	@Override
+	public String toString() {
+		return "NotaFiscal: Nome Cliente = " + nomeCliente + ", Valor Nota = " + valorNota + ", Valor Imposto = " + valorImposto;
+	}		
 		
 }
--- geradorNotaFiscal/test/NotaFiscalTest.java
+++ geradorNotaFiscal/test/NotaFiscalTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import gerador.NotaFiscal;
+
 class NotaFiscalTest {
 
 	NotaFiscal nf1;
@@ -32,5 +34,59 @@ class NotaFiscalTest {
 	void testNomeCliente2() {
 		assertNotEquals(nf2.getNomeCliente(), "Mari");
 	}
+	
+	@Test
+	void testValorNota1() {
+		assertEquals(nf3.getValorNota(), 100.00);
+	}
+	
+	@Test
+	void testValorNota2() {
+		assertNotEquals(nf2.getValorNota(), 400.00);
+	}
+	
+	@Test
+	void testValorImposto1() {
+		assertEquals(nf3.getValorImposto(), 6.00);
+	}
+	
+	@Test
+	void testValorImposto2() {
+		assertNotEquals(nf2.getValorImposto(), 10.00);
+	}
+	
+
+	@Test
+	void testSetNome() {
+		nf1.setNomeCliente("Joaquim");
+		assertEquals(nf1.getNomeCliente(), "Joaquim");
+	}
+	
+	@Test
+	void testSetValor() {
+		nf2.setValorImposto(50.00);
+		assertEquals(nf2.getValorImposto(), 50.00);
+	}
+	
+	@Test
+	void testVazio() {
+		try {
+			NotaFiscal nf4 = new NotaFiscal("", 1000, 250);
+			fail();
+		} catch (IllegalArgumentException iae) {
+			assertEquals("Informações não podem ser vazios.", iae.getMessage());
+		}
+	}
+
+	@Test
+	void testNulo() {
+		try {
+			NotaFiscal nf5 = new NotaFiscal(null, 1000, 250);
+			fail();
+		} catch (NullPointerException npe) {
+			assertEquals("Informações não podem ser nulos.", npe.getMessage());
+		}
+	}
+	
 
 }
